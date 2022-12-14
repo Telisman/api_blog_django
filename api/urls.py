b38ef7e4a43c92493605c23da0bce330151f0969
Davor Telisman <davortelisman@gmail.com>
2022-12-14 13:31:02 +0100

    Create page with all blog posts, and create page for blog detail view. after clicking on the post link, the page will redirect to blog_detail page. Where you can read full blog content

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
-from .views import LoginView,blog,BlogDetailView
+from .views import LoginView,blog,BlogDetailView,UpdateViewBlog
 
 urlpatterns = [
     path('users/', views.UserList.as_view()),
@@ -12,7 +12,8 @@ urlpatterns = [
     path('logandreg/', include('django.contrib.auth.urls')),
     path('logandreg/', LoginView.as_view(), name='logandreg'),
     path('blog/', blog, name='blog'),
-    path('detail/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/detail/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/detail/edit/<int:pk>', UpdateViewBlog.as_view(), name='update_blog'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
