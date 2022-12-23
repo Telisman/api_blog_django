9285bd842521d15e9bbb4515828d5b1fbe1b4e99
Davor Telisman <davortelisman@gmail.com>
2022-12-23 12:17:34 +0100

    Change urls files, to set home page on address  http://127.0.0.1:8000/

--- api_blog_django/urls.py
+++ api_blog_django/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from homepage.views import HomePageView
 urlpatterns = [
+    path('', HomePageView, name='home'),
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('', include('django.contrib.auth.urls')),
-    path('', include('homepage.urls')),
+    # path('', include('homepage.urls')),
 ]
