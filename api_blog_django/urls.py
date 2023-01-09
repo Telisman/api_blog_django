11123e6a903c06026e0f3f5ab1569d3c110b3070
Davor Telisman <davortelisman@gmail.com>
2023-01-09 09:10:55 +0100

    Add function for web development in settings.py file.

--- api_blog_django/urls.py
+++ api_blog_django/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path,include
 from homepage.views import HomePageView
@@ -7,5 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('', include('django.contrib.auth.urls')),
-    # path('', include('homepage.urls')),
 ]
